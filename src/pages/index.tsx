85dd7d0484bf7b6a917d577809419a217c1edee2
rajneeshsingh1007@gmail.com <rajneeshsingh1007@gmail.com>
2023-02-13 03:45:41 +0530

    added page background color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,8 @@ export default function Home() {
     setCounter(counter - 1)
   }
   return (
-    <div className='bg-blue-900 min-h-screen'>
+    <div className='bg-[#0f1129]
+    min-h-screen'>
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
